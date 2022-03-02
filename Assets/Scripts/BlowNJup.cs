5379ccb8a4053d6844525ee190d3e82791f8533f
sshamsSVA <sshams@svaprogram.org>
2022-03-02 14:10:17 -0500

    updates

--- Assets/Scripts/BlowNJup.cs
+++ Assets/Scripts/BlowNJup.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BlowNJup : MonoBehaviour
 {
+    Vector3 offset = new Vector3(5,5,0);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,10 @@ public class BlowNJup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 5.0f);
+
+        //Debug.Log(hit.transform);
+        Debug.DrawRay(transform.position + offset, Vector2.up * 50.0f, Color.red);
     }
 
     IEnumerator Kaboom()
