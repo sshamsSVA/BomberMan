b47618c8b1924bd56a513400968279868c9aaaf9
sshamsSVA <sshams@svaprogram.org>
2022-03-08 08:21:14 -0500

    trying to get the bomb to explode in a path

--- Assets/Scripts/BlowNJup.cs
+++ Assets/Scripts/BlowNJup.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class BlowNJup : MonoBehaviour
 {
-    Vector3 offset = new Vector3(5,5,0);
+    
+
+    public float distance;
+
+    [SerializeField] Transform castPoint;
+    
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("Kaboom");
+        StartCoroutine("DetectBlocks");
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 5.0f);
+        
 
-        //Debug.Log(hit.transform);
-        Debug.DrawRay(transform.position + offset, Vector2.up * 50.0f, Color.red);
+    
     }
 
     IEnumerator Kaboom()
     {
         yield return new WaitForSeconds(5);
         Destroy(gameObject);
+    }
+
+    IEnumerator DetectBlocks()
+    {
+        yield return null;
     }
 }
